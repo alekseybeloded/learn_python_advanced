257764a41e28b98e27ff2581b91659446962d7eb
AlexeyBeloded <belod-aleksej@yandex.ru>
2023-05-22 21:47:56 +0300

    feat: add new tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,37 @@
-from main import start_condition, draw_board
+from main import start_draw_board, create_board, draw_board, check_win_extracted, check_win
 
-def test_start_condition_return_correct_board():
-    assert start_condition() == ' | | \n=====\n | | \n=====\n | | ' 
+def test__start_draw_board__return_start_board():
+    assert start_draw_board() == '1|2|3\n=====\n4|5|6\n=====\n7|8|9\nВыше изображена игральная доска с номерами возможных ходов\n' 
 
-def test_draw_board_return_correct_board_state1():
+def test__create_board__return_empty_board():
+    assert create_board() == [' '] * 9
+
+def test__draw_board__return_board_if_x_on_board():
     assert draw_board(['X',' ',' ',' ',' ',' ',' ',' ',' ']) == 'X| | \n=====\n | | \n=====\n | | ' 
 
-def test_draw_board_return_new_corrrect_board_state2():
-    assert draw_board([' ','X',' ',' ',' ',' ',' ',' ',' ']) == ' |X| \n=====\n | | \n=====\n | | '     
+def test__draw_board__return_board_if_o_on_board():
+    assert draw_board(['O',' ',' ',' ',' ',' ',' ',' ',' ']) == 'O| | \n=====\n | | \n=====\n | | '     
 
-def test_draw_board_return_new_corrrect_state3():
+def test__draw_board__return_board_if_x_and_o_on_board():
     assert draw_board([' ','X','O',' ','X',' ','O',' ',' ']) == ' |X|O\n=====\n |X| \n=====\nO| | '
+
+def test__check_win_extracted__return_if_win_at_row():
+    assert check_win_extracted(['X','X','X',' ',' ',' ',' ',' ',' '], 'X') == True
+
+def test__check_win_extracted__return_if_win_at_column():
+    assert check_win_extracted(['X',' ',' ','X',' ',' ','X',' ',' '], 'X') == True
+
+def test__check_win_extracted__return_if_win_at_crossline():
+    assert check_win_extracted(['X',' ',' ',' ','X',' ',' ',' ','X'], 'X') == True
+
+def test__check_win_extracted__return_if_not_win():
+    assert check_win_extracted(['X',' ',' ',' ',' ',' ','X',' ',' '], 'X') == False
+
+def test__check_win__return_if_user_win():
+    assert check_win(['X','X','X',' ',' ',' ',' ',' ',' '], 'X') == 'Победил пользователь!'
+
+def test__check_win__return_if_pc_win():
+    assert check_win(['X','X','X',' ',' ',' ',' ',' ',' '], 'O') == 'Победил компьютер!'
+
+def test__check_win__return_if_user_and_pc_not_win():
+    assert check_win(['X','X',' ',' ',' ',' ',' ',' ',' '], 'O') == None
